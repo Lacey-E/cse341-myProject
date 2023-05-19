0bfa5c721eefe26ab49fa3cd62a4ca19ee02dafa
Lacey-E <buchiezim@gmail.com>
2023-05-18 18:12:46 -0600

    first

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My Blog Headlines API',
+    description: 'Contact Api'
+  },
+  host: 'esther-node-js-class1.onrender.com',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
